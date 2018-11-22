49e5a30d231c550125ebe4b6ff1c50bfab48bc4d
mykkee3 <kneely1250@gmai.com>
2018-11-22 00:56:39 -0500

    initial commit
    adding manifest
    starting update.sh

--- update_tmp.sh
+++ update_tmp.sh
@@ -1,3 +1,8 @@
 echo "\nStarting Update shell"
 
-echo "Update shell Done"
+cur_ver=$(jq --raw-output '.info.version' manifest.json);
+#new_ver=$(curl "" | jq --raw-output '.info.version')
+
+echo "\nCurrent version: $cur_ver\nExpected version: $new_ver"
+
+echo "\nUpdate shell Done"
