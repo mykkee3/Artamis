261721d179257bdf16f98a9f5157e705b1376f1a
mykkee3 <kneely1250@gmail.com>
2018-12-17 21:39:55 -0500

    Update TODO.sh

--- TODO.sh
+++ TODO.sh
@@ -8,22 +8,85 @@
 #		
 #	TODO.sh
 #		- make shell script that edits its own comments to colate all TODO statements
-
 #
 #
 #
 # [x] add - GUI.popcorn (popup at bottom)
 # [ ] add - button click animation
-# [ ] add - face animations
+# [x] add - face animations
 # [x] add - auto menu builder
 # [x] add - viewport draw priority
 #
+# [ ] add - I2c to arduino
+#     [ ] Tests:
+#         [ ] LED on/off
+#         [ ] Move wing
 #
+# [ ] add - JS to ArduinoNano I/O serial communication
+#     [ ] JS to Python
+#     [ ] Python to Nano I2c 
+#     [ ] I2c to Nano I/O 
+# 
+# [ ] add - face animations:
+#     [x] idle
+#     [x] Blink
+#     [ ] Talk
+#     [ ] Supprised
+#     [ ] Embarrased
+#     [ ] Laugh
+#     [ ] Intrigued
+#     [ ] Confuzed
+#     [ ] Lying
+#     [ ] Shifty
+#     [ ] Guilty
+#     [ ] Scared
+#     [ ] Bored
+#     [ ] Mad:
+#         [ ] Upset
+#         [ ] Angry
+#         [ ] Distraught
+#     [ ] Sad:
+#         [ ] Upset
+#         [ ] Sad
+#         [ ] Dissapointed
+#         [ ] Crying
+#     [ ] Lewd:
+#         [ ] Bedroom Eyes
+#         [ ] Longing Bedroom Eyes
+#         [ ] Eroused
+#         [ ] Being pleasured
+#         [ ] Enjoying sex
+#         [ ] Being dissapointed
+#         [ ] Hurts
+#         [ ] Suprised
+#         [ ] Shy
+#         [ ] Angry
+#         [ ] Getting Close
+#         [ ] Cumming Light
+#         [ ] Cumming Normal
+#         [ ] Cumming Hard
+#     [ ] Shy:
+#         [ ] Happy
+#         [ ] Sad
+#         [ ] Scared
+#     [ ] Happy:
+#         [ ] Fun
+#         [ ] Happy
+#         [ ] Giggling
 #
-#
-#
-#
-#
+# [ ] add - Viewport management PiP screen management
+# [ ] and - viewport animations:
+#     [ ] sex minigame:
+#         [ ] show butt
+#         [ ] show pleasureing device:
+#             [ ] dick
+#             [ ] mouse finger
+#         [ ] cumming animation
+#         [ ] creampie
+#     [ ] show weather
+#     [ ] wolfram answers
+#     [ ] time
+#     [ ] computation projects (artamis runs other projects remotely)
 #
 #
 #
