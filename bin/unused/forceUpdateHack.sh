206f8457de416467630676acdd08059863cb719a
mykkee3 <kneely1250@gmail.com>
2019-03-22 18:13:15 -0400

    Update forceUpdateHack.sh
    added reboot

--- bin/unused/forceUpdateHack.sh
+++ bin/unused/forceUpdateHack.sh
@@ -3,8 +3,9 @@
 cd /home/pi/Desktop/dev/Artamis/
 
 # Set Variables #
+# WARNING: probably wont work with 0 or . in the p place... so dont do it; besides a version '.0' should not need be force updated.
 p=4; #Place in the version to decrement including the dot
-# WARNING: probably wont work with 0 or . so dont do it; besides a version '.0' should not need be force updated
+do_reboot=true;
 
 cur_ver=$(jq --raw-output '.info.version' manifest.json);
 new_ver=${cur_ver:0:p}$((${cur_ver:p}-1));
@@ -15,3 +16,6 @@ echo "changing ~/manifest.json/.info.version from $cur_ver to $new_ver.";
 
 # Switch the Versions #
 sed -i 's/$cur_ver/$new_ver/' manifest.json;
+
+# Reboot #
+if [ $do_reboot ] then sudo reboot; fi
