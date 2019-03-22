b4b677b8880dc52843bd0945e2b3400375349399
mykkee3 <kneely1250@gmail.com>
2019-03-22 15:33:53 -0400

    Update forceUpdateHack.sh

--- bin/unused/forceUpdateHack.sh
+++ bin/unused/forceUpdateHack.sh
@@ -1,12 +1,11 @@
-#cd /home/pi/Desktop/dev/Artamis/
-cd /e/dev/Artamis/Artamis
+cd /home/pi/Desktop/dev/Artamis/
+#cd /e/dev/Artamis/Artamis
+
+p=4
 
 cur_ver=$(jq --raw-output '.info.version' manifest.json);
-echo $cur_ver
+new_ver="${cur_ver:0:p}((${cur_ver:p}-1))"
 
-n=`-n $str | tail -c 1`
-n2='${n-1}'
-echo $n $n2
+echo "changing version $cur_ver to $new_ver"
 
-
-
+sed 's/${cur_ver}/${new_ver}/' manifest.json
