6c5a72660fed5b814f038c6c34912b14a5cd663a
mykkee3 <kneely1250@gmail.com>
2019-03-22 18:37:31 -0400

    Update forceUpdateHack.sh

--- bin/unused/forceUpdateHack.sh
+++ bin/unused/forceUpdateHack.sh
@@ -5,7 +5,7 @@ cd /home/pi/Desktop/dev/Artamis/
 # Set Variables #
 # WARNING: probably wont work with 0 or . in the p place... so dont do it; besides a version '.0' should not need be force updated.
 p=4; #Place in the version to decrement including the dot
-do_reboot=true;
+do_reboot=false;
 
 cur_ver=$(jq --raw-output '.info.version' manifest.json);
 new_ver=${cur_ver:0:p}$((${cur_ver:p}-1));
