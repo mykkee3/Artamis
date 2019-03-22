45009efba2b79c7f3e23d35e9eab5739393f5d80
mykkee3 <kneely1250@gmail.com>
2019-03-22 18:09:53 -0400

    Update forceUpdateHack.sh
    should be the final fix before the primary update system takes over

--- bin/unused/forceUpdateHack.sh
+++ bin/unused/forceUpdateHack.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+# WARNING: Will not work if run with sh; run with bash, eg: `bash forceUpdateHack.sh`
 cd /home/pi/Desktop/dev/Artamis/
-#cd /e/dev/Artamis/Artamis
 
-p=4
+# Set Variables #
+p=4; #Place in the version to decrement including the dot
+# WARNING: probably wont work with 0 or . so dont do it; besides a version '.0' should not need be force updated
 
 cur_ver=$(jq --raw-output '.info.version' manifest.json);
-echo cur_ver;
-new_ver="${cur_ver:0:p}$((${cur_ver:p}-1))";
+new_ver=${cur_ver:0:p}$((${cur_ver:p}-1));
 
-echo "changing version $cur_ver to $new_ver";
+# Some Printing #
+echo "~/bin/unused/forceUpdateHack.sh";
+echo "changing ~/manifest.json/.info.version from $cur_ver to $new_ver.";
 
-sed 's/${cur_ver}/${new_ver}/' manifest.json;
+# Switch the Versions #
+sed -i 's/$cur_ver/$new_ver/' manifest.json;
