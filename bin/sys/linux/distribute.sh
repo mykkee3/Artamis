1a802bc4bcd34daa5a09115630e05b986f115b4c
mykkee3 <kneely1250@gmail.com>
2019-03-23 21:30:57 -0400

    fixing auto updating

--- bin/sys/linux/distribute.sh
+++ bin/sys/linux/distribute.sh
@@ -4,4 +4,12 @@
 #
 # INFO: The info for this folder can be found in /../README.md
 
-cd /home/pi/Desktop/dev/Artamis/
+sys_dir="/home/Artamis/"
+
+cd /home/pi/Desktop/dev/Artamis/bin/sys/
+
+sudo cp autoArtamis.desktop ~/.config/autostart/
+
+mkdir /home/Artamis/
+sudo cp artamis.sh $sys_dir
+sudo cp update.sh $sys_dir
