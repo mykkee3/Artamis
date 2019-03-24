52b54870d6971adcbe286f51457bb52a4f0f0945
mykkee3 <kneely1250@gmail.com>
2019-03-23 21:20:50 -0400

    moving

---
+++ bin/sys/linux/distribute.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This file distributes all of the linux system files around the file system ass needed
+#
+# INFO: The info for this folder can be found in /../README.md
+
+cd /home/pi/Desktop/dev/Artamis/
