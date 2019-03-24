49969a546fd14007c51e80674de52ed7088ce04d
mykkee3 <kneely1250@gmail.com>
2019-03-23 22:13:03 -0400

    add IO

--- bin/sys/linux/artamis.sh
+++ bin/sys/linux/artamis.sh
@@ -1,4 +1,5 @@
 # Start artamis
 
 cd /home/pi/Desktop/dev/Artamis/
-sh run.sh &
+#sh run.sh &
+sh bin/tests/IO/run.sh
---
+++ bin/tests/IO/run.sh
@@ -0,0 +1,5 @@
+
+
+# runs IO test for I2C connection to arduino
+
+echo "Starting I2C connection test"
